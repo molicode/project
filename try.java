91f695a25a449220585e6b3278d6d22c52a033c7
Luis Molina <molicode1347@gmail.com>
2023-11-24 08:11:58 -0300

    Update try.java

--- try.java
+++ try.java
@@ -1,74 +1,419 @@
-======= try this 
-import com.opencsv.bean.ColumnPositionMappingStrategy;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
-import org.springframework.stereotype.Service;
-
-import java.io.StringWriter;
-import java.util.List;
-
-@Service
-public class CsvService {
-
-    public <T> String generateCsv(List<T> list, Class<T> type) {
-        try (StringWriter writer = new StringWriter()) {
-            ColumnPositionMappingStrategy<T> mappingStrategy = new ColumnPositionMappingStrategy<>();
-            mappingStrategy.setType(type);
-
-            // Define las cabeceras a partir de los nombres de los campos de la clase T
-            // Esto requiere un método adicional para obtener los nombres de los campos
-
-            StatefulBeanToCsv<T> beanToCsv = new StatefulBeanToCsvBuilder<T>(writer)
-                    .withMappingStrategy(mappingStrategy)
-                    .build();
-
-            beanToCsv.write(list);
-            return writer.toString();
-        } catch (Exception e) {
-            // Manejar la excepción como sea necesario
-            return null;
-        }
-    }
-}
-
-
-========== test
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-@ExtendWith(SpringExtension.class)
-public class CsvControllerTest {
+public class ProductLabelSkillControllerTest {
 
     @Mock
-    private CsvService csvService;
+    private ProductLabelSkillService productLabelSkillService;
 
     @InjectMocks
-    private CsvController csvController;
+    private ProductLabelSkillController productLabelSkillController;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
-    public void testGenerateCsv() {
-        List<MyObject> myList = Arrays.asList(new MyObject(), new MyObject());
-        String expectedCsv = "id,name\n1,Example\n2,Example2";
-        
-        when(csvService.generateCsv(anyList(), eq(MyObject.class))).thenReturn(expectedCsv);
-        
-        ResponseEntity<String> response = csvController.generateCsv(myList);
-        
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(expectedCsv, response.getBody());
+    public void testGetAllProductLabelSkills() {
+        // Arrange
+        List<ProductLabelSkill> expectedSkills = new ArrayList<>();
+        // Supongamos que el servicio devuelve una lista vacía
+        when(productLabelSkillService.getAllProductLabelSkills()).thenReturn(expectedSkills);
 
-        verify(csvService).generateCsv(anyList(), eq(MyObject.class));
+        // Act
+        ResponseEntity<List<ProductLabelSkill>> response = productLabelSkillController.getAllProductLabelSkills();
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        verify(productLabelSkillService).getAllProductLabelSkills();
+        assertEquals(expectedSkills, response.getBody());
+    }
+
+    // Aquí continuarías con pruebas para los otros métodos...
+}
+
+
+// ... (imports previamente mencionados)
+
+public class ProductLabelSkillControllerTest {
+
+    // ... (Mocks e InjectMocks como antes)
+
+    // ... (Método init como antes)
+
+    // ... (Prueba para getAllProductLabelSkills como antes)
+
+    @Test
+    public void testCreateProductLabelSkill() {
+        // Arrange
+        ProductLabelSkillDto productLabelSkillDto = new ProductLabelSkillDto(); // Suponiendo que esta es la clase DTO adecuada
+        when(productLabelSkillService.createProductLabelSkill(productLabelSkillDto)).thenReturn(productLabelSkillDto);
+
+        // Act
+        ResponseEntity<ProductLabelSkillDto> response = productLabelSkillController.createProductLabelSkill(productLabelSkillDto);
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        verify(productLabelSkillService).createProductLabelSkill(productLabelSkillDto);
+        assertEquals(productLabelSkillDto, response.getBody());
+    }
+
+    @Test
+    public void testCreateProductLabelSkill_ThrowsException() {
+        // Arrange
+        ProductLabelSkillDto productLabelSkillDto = new ProductLabelSkillDto();
+        when(productLabelSkillService.createProductLabelSkill(productLabelSkillDto)).thenThrow(new RuntimeException("Error"));
+
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> productLabelSkillController.createProductLabelSkill(productLabelSkillDto));
+    }
+
+    @Test
+    public void testModifyProductLabelSkill() {
+        // Arrange
+        ProductLabelSkillDto productLabelSkillDto = new ProductLabelSkillDto();
+        when(productLabelSkillService.modifyProductLabelSkill(productLabelSkillDto)).thenReturn(productLabelSkillDto);
+
+        // Act
+        ResponseEntity<ProductLabelSkillDto> response = productLabelSkillController.modifyProductLabelSkill(productLabelSkillDto);
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        verify(productLabelSkillService).modifyProductLabelSkill(productLabelSkillDto);
+        assertEquals(productLabelSkillDto, response.getBody());
+    }
+
+    @Test
+    public void testModifyProductLabelSkill_ThrowsException() {
+        // Arrange
+        ProductLabelSkillDto productLabelSkillDto = new ProductLabelSkillDto();
+        when(productLabelSkillService.modifyProductLabelSkill(productLabelSkillDto)).thenThrow(new RuntimeException("Error"));
+
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> productLabelSkillController.modifyProductLabelSkill(productLabelSkillDto));
+    }
+
+    @Test
+    public void testDeleteProductLabelSkill() {
+        // Arrange
+        long skillId = 1L; // Supongamos que este es el ID del skill
+        doNothing().when(productLabelSkillService).deleteProductLabelSkill(skillId);
+
+        // Act
+        ResponseEntity<String> response = productLabelSkillController.deleteProductLabelSkill(skillId);
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        verify(productLabelSkillService).deleteProductLabelSkill(skillId);
+    }
+
+    @Test
+    public void testDeleteProductLabelSkill_ThrowsException() {
+        // Arrange
+        long skillId = 1L;
+        doThrow(new RuntimeException("Error")).when(productLabelSkillService).deleteProductLabelSkill(skillId);
+
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> productLabelSkillController.deleteProductLabelSkill(skillId));
+    }
+
+    // ... (Posibles pruebas adicionales para otros métodos)
+}
+===============================
+
+    import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import java.util.Optional;
+
+public class SkillControllerTest {
+
+    @Mock
+    private SkillService skillService;
+
+    @InjectMocks
+    private SkillController skillController;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testGetAllSkills() {
+        // Arrange
+        List<SkillDto> expectedSkills = new ArrayList<>();
+        when(skillService.getAllSkills()).thenReturn(expectedSkills);
+
+        // Act
+        ResponseEntity<List<SkillDto>> response = skillController.getAllSkills();
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        verify(skillService).getAllSkills();
+        assertEquals(expectedSkills, response.getBody());
+    }
+
+    @Test
+    public void testGetAllSkills_ThrowsException() {
+        // Arrange
+        when(skillService.getAllSkills()).thenThrow(new RuntimeException("Error"));
+
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> skillController.getAllSkills());
+    }
+
+    @Test
+    public void testGetSkillByName() {
+        // Arrange
+        String skillName = "Java";
+        SkillDto skillDto = new SkillDto(); // Suponiendo que esta es la clase DTO adecuada
+        when(skillService.getSkillByName(skillName)).thenReturn(Optional.of(skillDto));
+
+        // Act
+        ResponseEntity<SkillDto> response = skillController.getSkillByName(skillName);
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        verify(skillService).getSkillByName(skillName);
+        assertEquals(skillDto, response.getBody());
+    }
+
+    @Test
+    public void testGetSkillByName_NotFound() {
+        // Arrange
+        String skillName = "Java";
+        when(skillService.getSkillByName(skillName)).thenReturn(Optional.empty());
+
+        // Act
+        ResponseEntity<SkillDto> response = skillController.getSkillByName(skillName);
+
+        // Assert
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        verify(skillService).getSkillByName(skillName);
+        assertNull(response.getBody());
+    }
+
+    @Test
+    public void testGetSkillByName_ThrowsException() {
+        // Arrange
+        String skillName = "Java";
+        when(skillService.getSkillByName(skillName)).thenThrow(new RuntimeException("Error"));
+
+        // Act & Assert
+        assertThrows(RuntimeException.class, () -> skillController.getSkillByName(skillName));
+    }
+
+    // Similar tests would be written for getSkillByProductName, getSkillByIdName
+    // and for modifySkill and addSkill including success and exception scenarios.
+
+    // ... (Posibles pruebas adicionales para otros métodos)
+}
+
+
+======================== 
+
+    import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+public class DataServiceTest {
+
+    @Mock
+    private CsvMapperService csvMapperService;
+    @Mock
+    private CsvExportService csvExportService;
+    @InjectMocks
+    private DataService dataService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testExportData_UserFeedback_Success() throws IllegalAccessException {
+        // Arrange
+        QueryCsvDto queryCsvDto = mock(QueryCsvDto.class);
+        when(queryCsvDto.getAlias()).thenReturn("USERFEEDBACK");
+        when(csvExportService.exportData(any())).thenReturn(new byte[0]);
+
+        // Act
+        ResponseEntity<?> result = dataService.exportData(queryCsvDto);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(200, result.getStatusCodeValue());
+    }
+
+    // Similar tests for other aliases and scenarios...
+
+    @Test(expected = IllegalAccessException.class)
+    public void testExportData_ThrowsIllegalAccessException() throws IllegalAccessException {
+        // Arrange
+        QueryCsvDto queryCsvDto = mock(QueryCsvDto.class);
+        when(queryCsvDto.getAlias()).thenReturn("INVALID");
+        when(csvExportService.exportData(any())).thenThrow(IllegalAccessException.class);
+
+        // Act
+        dataService.exportData(queryCsvDto);
+    }
+
+    // Other tests...
+}
+
+// Continuación de DataServiceTest
+
+@Test
+public void testExportUserFeedbackData_Success() throws IllegalAccessException {
+    // Arrange
+    QueryCsvDto queryCsvDto = mock(QueryCsvDto.class);
+    // Suponer que setupMockQueryCsvDto configura el objeto mock apropiadamente
+    setupMockQueryCsvDto(queryCsvDto, "some feedback data setup");
+    byte[] mockData = new byte[] {1, 2, 3};
+    when(csvExportService.exportData(any())).thenReturn(mockData);
+
+    // Act
+    ResponseEntity<?> result = dataService.exportUserFeedbackData(queryCsvDto);
+
+    // Assert
+    assertNotNull(result);
+    assertEquals(200, result.getStatusCodeValue());
+    // Assert the headers or body if necessary
+}
+
+@Test(expected = IllegalAccessException.class)
+public void testExportUserFeedbackData_ThrowsIllegalAccessException() throws IllegalAccessException {
+    // Arrange
+    QueryCsvDto queryCsvDto = mock(QueryCsvDto.class);
+    when(csvExportService.exportData(any())).thenThrow(IllegalAccessException.class);
+
+    // Act
+    dataService.exportUserFeedbackData(queryCsvDto);
+}
+
+@Test
+public void testExportDataQuery_Success() throws IllegalAccessException {
+    // Arrange
+    QueryCsvDto queryCsvDto = mock(QueryCsvDto.class);
+    setupMockQueryCsvDto(queryCsvDto, "some data query setup");
+    byte[] mockData = new byte[] {1, 2, 3};
+    when(csvExportService.exportData(any())).thenReturn(mockData);
+
+    // Act
+    ResponseEntity<?> result = dataService.exportDataQuery(queryCsvDto);
+
+    // Assert
+    assertNotNull(result);
+    assertEquals(200, result.getStatusCodeValue());
+    // Assert the headers or body if necessary
+}
+
+@Test(expected = IllegalAccessException.class)
+public void testExportDataQuery_ThrowsIllegalAccessException() throws IllegalAccessException {
+    // Arrange
+    QueryCsvDto queryCsvDto = mock(QueryCsvDto.class);
+    when(csvExportService.exportData(any())).thenThrow(IllegalAccessException.class);
+
+    // Act
+    dataService.exportDataQuery(queryCsvDto);
+}
+
+// Helper method for setting up QueryCsvDto mocks
+private void setupMockQueryCsvDto(QueryCsvDto queryCsvDto, String setupDetails) {
+    // Configuration logic for the mock QueryCsvDto based on the setupDetails
+}
+==================
+
+    import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Collections;
+
+public class CsvExportServiceTest {
+
+    private CsvExportService csvExportService;
+
+    @Before
+    public void setUp() {
+        csvExportService = new CsvExportService(...); // Suponiendo que tienes una lista de estrategias
+    }
+
+    @Test
+    public void testExportCsv_Success() throws IllegalAccessException {
+        // Arrange
+        List<SomeItem> items = Collections.singletonList(new SomeItem());
+
+        // Act
+        byte[] csvData = csvExportService.exportCsv(items);
+
+        // Assert
+        assertNotNull(csvData);
+        // Verificar el contenido del CSV si es posible
+    }
+
+    @Test(expected = IllegalAccessException.class)
+    public void testExportCsv_NoStrategy() throws IllegalAccessException {
+        // Arrange
+        List<SomeOtherItem> items = Collections.singletonList(new SomeOtherItem());
+
+        // Act
+        csvExportService.exportCsv(items);
+    }
+
+    // Other tests...
+}
+
+// Continuación de CsvExportServiceTest
+
+@Test
+public void testExportCsv_EmptyList() {
+    // Arrange
+    List<Object> emptyList = Collections.emptyList();
+
+    // Act & Assert
+    try {
+        csvExportService.exportCsv(emptyList);
+        fail("Should have thrown an IllegalAccessException.");
+    } catch (IllegalAccessException e) {
+        // Expected exception
+        assertEquals("The item list cannot be null or empty.", e.getMessage());
     }
 }
+
+@Test
+public void testExportCsv_NullList() {
+    // Act & Assert
+    try {
+        csvExportService.exportCsv(null);
+        fail("Should have thrown an IllegalAccessException.");
+    } catch (IllegalAccessException e) {
+        // Expected exception
+        assertEquals("The item list cannot be null or empty.", e.getMessage());
+    }
+}
+
+// If there are more item types that CsvExportService can handle, you would add additional tests here
+
+
+
+===============
